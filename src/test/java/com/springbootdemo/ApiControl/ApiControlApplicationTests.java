fe481b746de5f3838cc239428470d4f4cac9de4b
Islasmar <marianoislasfajardo@gmail.com>
2024-03-06 10:23:11 -0600

    Primer commit

---
+++ src/test/java/com/springbootdemo/ApiControl/ApiControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdemo.ApiControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
